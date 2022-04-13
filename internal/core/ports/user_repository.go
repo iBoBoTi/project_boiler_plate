d3aac27589c6ccf761705300b13b878f6a6c5e53
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-13 23:00:27 +0100

    doc: add the .idea and .env file to the .gitignore file

---
+++ internal/core/ports/user_repository.go
@@ -0,0 +1,6 @@
+// Package ports contains interfaces for the adapters to communicate to the core.
+package ports
+
+// UserRepository is the interface for the core to communicate with the adapters usecase.
+type UserRepository interface {
+}
---
+++ internal/core/ports/user_service.go
@@ -0,0 +1,5 @@
+package ports
+
+// UserService is a service interface for the core to communicate with the adapters handlers .
+type UserService interface {
+}
