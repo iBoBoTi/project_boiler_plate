f7b17d7c00e1eaa3ceba6072d13d0138e54b9ae7
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-19 19:46:39 +0100

    feat: add the user handler interface

--- internal/core/ports/user_handler.go
+++ internal/core/ports/user_handler.go
@@ -1,1 +1,11 @@
 package ports
+
+import "github.com/gin-gonic/gin"
+
+type UserHandler interface {
+	GetUserByID(c *gin.Context)
+	GetUsersList(c *gin.Context)
+	CreateUser(c *gin.Context)
+	UpdateUser(c *gin.Context)
+	DeleteUser(c *gin.Context)
+}
