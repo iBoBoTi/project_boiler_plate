35402304fe385b41804547d97bac6c1db57e522f
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-19 19:45:42 +0100

    feat: add the user service interface

--- internal/core/ports/user_service.go
+++ internal/core/ports/user_service.go
@@ -1,5 +1,11 @@
 package ports
 
+import "github.com/iBoBoTi/project_boiler_plate/internal/core/domain"
+
 // UserService is a service interface for the core to communicate with the adapters handlers .
 type UserService interface {
+	GetUser() (*domain.User, error)
+	CreateUser() (*domain.User, error)
+	UpdateUser() (*domain.User, error)
+	DeleteUser() error
 }
