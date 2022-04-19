f7aa9e744cad443e445574112ec12c0499838874
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-19 21:01:18 +0100

    feat: add the log factory

--- pkg/log/log_factory.go
+++ pkg/log/log_factory.go
@@ -1,1 +1,24 @@
 package log
+
+import (
+	"errors"
+	"github.com/iBoBoTi/project_boiler_plate/internal/core/ports"
+)
+
+const (
+	InstanceLogrusLogger int = iota
+)
+
+var (
+	errInvalidLoggerInstance = errors.New("invalid log instance")
+)
+
+// NewLoggerFactory returns a logger interface type based on the provided logger instance
+func NewLoggerFactory(instance int) (ports.Logger, error) {
+	switch instance {
+	case InstanceLogrusLogger:
+		return NewLogrusLogger(), nil
+	default:
+		return nil, errInvalidLoggerInstance
+	}
+}
