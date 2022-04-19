abacc420b595e4d7178b3a788a4e0f21a3971b25
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-19 17:13:20 +0100

    feat: add the user repository interface

--- internal/core/ports/user_repository.go
+++ internal/core/ports/user_repository.go
@@ -1,6 +1,12 @@
 // Package ports contains interfaces for the adapters to communicate to the core.
 package ports
 
+import "github.com/iBoBoTi/project_boiler_plate/internal/core/domain"
+
 // UserRepository is the interface for the core to communicate with the adapters usecase.
 type UserRepository interface {
+	GetUser() (*domain.User, error)
+	CreateUser() (*domain.User, error)
+	UpdateUser() (*domain.User, error)
+	DeleteUser() error
 }
