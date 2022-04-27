f242602a38fb6e57e00b445deae3e9a99ecc5e4a
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-28 00:39:21 +0100

    feat: add the role handler interface

--- internal/core/ports/role_handler.go
+++ internal/core/ports/role_handler.go
@@ -1,1 +1,14 @@
 package ports
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type RoleHandler interface {
+	GetRole(c *gin.Context)
+	GetRoles(c *gin.Context)
+	CreateRole(c *gin.Context)
+	AddPermissionToRole(c *gin.Context)
+	RemovePermissionFromRole(c *gin.Context)
+	DeleteRole(c *gin.Context)
+}
