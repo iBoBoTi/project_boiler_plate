4537ce875b9ddca08eff9cf3590107fc49ebfef6
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-28 00:40:06 +0100

    feat: add the role repository interface

--- internal/core/ports/role_repository.go
+++ internal/core/ports/role_repository.go
@@ -1,1 +1,13 @@
 package ports
+
+import "github.com/iBoBoTi/project_boiler_plate/internal/core/domain"
+
+type RoleRepository interface {
+	GetRoleByName(name string) (*domain.Role, error)
+	GetAllRoles() ([]domain.Role, error)
+	GetRoleByID(id string) (*domain.Role, error)
+	CreateRole(role *domain.Role) error
+	AddPermission(id string, permission *domain.Permission) error
+	RemovePermission(id string, permission *domain.Permission) error
+	DeleteRole(id string) error
+}
