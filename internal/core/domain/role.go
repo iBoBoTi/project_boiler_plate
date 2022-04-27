a1a5798208f3c0e5da281fcfcebee8b1def016f5
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-27 23:56:21 +0100

    feat: add the role domain struct

--- internal/core/domain/role.go
+++ internal/core/domain/role.go
@@ -1,1 +1,12 @@
 package domain
+
+import "time"
+
+// Role determines the role a user plays in the application
+type Role struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
