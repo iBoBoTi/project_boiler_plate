a1dc04e66f12d4fe541397266b4e781f7072e0d3
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-28 20:30:31 +0100

    feat: add the permission handler interface

--- internal/core/ports/permission_handler.go
+++ internal/core/ports/permission_handler.go
@@ -1,1 +1,10 @@
 package ports
+
+import "github.com/gin-gonic/gin"
+
+type PermissionHandler interface {
+	CreatePermission(c *gin.Context)
+	DeletePermission(c *gin.Context)
+	GetAllPermissions(c *gin.Context)
+	GetPermissionByID(c *gin.Context)
+}
