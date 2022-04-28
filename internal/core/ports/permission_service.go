dea80f1140b4662d54b71d23b9f26fa6e4fc8f2b
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-28 20:31:40 +0100

    feat: add the permission service interface

--- internal/core/ports/permission_service.go
+++ internal/core/ports/permission_service.go
@@ -1,1 +1,10 @@
 package ports
+
+import "github.com/iBoBoTi/project_boiler_plate/internal/core/domain"
+
+type PermissionService interface {
+	CreatePermission(permission *domain.Permission) error
+	DeletePermission(id string) error
+	GetPermissionByID(id string) (*domain.Permission, error)
+	GetAllPermissions() ([]domain.Permission, error)
+}
