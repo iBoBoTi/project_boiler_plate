c99c20ee3c7964142794a003e8ba8c893d005a98
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-28 20:32:38 +0100

    feat: add the permission domain struct

--- internal/core/domain/permission.go
+++ internal/core/domain/permission.go
@@ -1,1 +1,11 @@
 package domain
+
+import "time"
+
+// Permission defines the role a user can carry out in the application
+type Permission struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
