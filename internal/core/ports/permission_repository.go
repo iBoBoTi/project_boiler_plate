7db8e23fbdf0b96a2235f31a943024bd32e0c304
iBoBoTi <iroegbucalistus@gmail.com>
2022-04-28 20:31:11 +0100

    feat: add the permission repository interface

--- internal/core/ports/permission_repository.go
+++ internal/core/ports/permission_repository.go
@@ -1,1 +1,11 @@
 package ports
+
+import "github.com/iBoBoTi/project_boiler_plate/internal/core/domain"
+
+type PermissionRepository interface {
+	CreatePermission(permission *domain.Permission) error
+	DeletePermission(id string) error
+	GetPermission(id string) (*domain.Permission, error)
+	GetPermissionByTitle(title string) (*domain.Permission, error)
+	GetAllPermissions() ([]domain.Permission, error)
+}
