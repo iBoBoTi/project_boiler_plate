98964b1012187c6ac932c3f47b9d47baf670ebe1
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-11 14:14:51 +0100

    feat: add the validation function for uuid and creating uuid for the ids

--- internal/core/domain/role.go
+++ internal/core/domain/role.go
@@ -5,8 +5,8 @@ import "time"
 // Role determines the role a user plays in the application
 type Role struct {
 	ID          string    `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
+	Title       string    `json:"title" validate:"required"`
+	Description string    `json:"description" validate:"required"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
